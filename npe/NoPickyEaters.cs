f5e89e2a3b983be0989fdbc2723439e98087b359
Starei <62623286+stareiglace@users.noreply.github.com>
2026-03-13 17:10:29 +1100

    Add files via upload

---
+++ npe/NoPickyEaters.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using Verse;
+
+namespace Starei.NoPickyEaters
+{
+    [StaticConstructorOnStartup]
+    public class NoPickyEaters : Mod
+    {
+        public NoPickyEaters(ModContentPack content)
+        : base(content)
+        {
+            new Harmony("Starei.NoPickyEaters").PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ npe/STOP_FUCKING_EATING_THE_UNNATURAL_CORPSE.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace Starei.NoPickyEaters
+{
+    [HarmonyPatch(typeof(Corpse), nameof(Corpse.IngestibleNow), MethodType.Getter)]
+    public class STOP_FUCKING_EATING_THE_UNNATURAL_CORPSE
+    {
+        [HarmonyPrefix]
+        public static bool Prefix(ref bool __result, Corpse __instance)
+        {
+            if(__instance is UnnaturalCorpse)
+            {
+                __result = false;
+                return false;
+            }
+            return true;
+        }
+    }
+}
